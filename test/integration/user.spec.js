8302be890fc6fb56fdaa6052509c678eb5a69885
Kifen <kifenraph@gmail.com>
2020-04-10 09:04:07 +0000

    write login integration test

--- test/integration/user.spec.js
+++ test/integration/user.spec.js
@@ -4,6 +4,7 @@ const dbHandler = require("../helpers/db_handler");
 const app = require("../../app");
 const { User } = require("../../lib/models");
 const { genMockUserData } = require("../helpers/mock");
+const faker = require("faker");
 
 const { expect } = chai;
 
@@ -11,10 +12,13 @@ chai.use(chaiHttp);
 
 describe("/api/vi/users", () => {
   const baseAPi = "/api/v1/users";
+  const invalidPassword = faker.internet.password();
   let user, userData;
 
   const execUser = async () => {
-    user = new User(userData);
+    user = new User({ email: userData.email, name: userData.name });
+    await user.setPassword(userData.password);
+    console.log();
     await user.save();
   };
 
@@ -45,8 +49,8 @@ describe("/api/vi/users", () => {
         });
     });
 
-    it("should return 400 if user already exists", (done) => {
-      execUser();
+    it("should return 400 if user already exists", async () => {
+      await execUser();
       chai
         .request(app)
         .post(`${baseAPi}/register`)
@@ -57,8 +61,47 @@ describe("/api/vi/users", () => {
             `User with email ${user.email} already exists.`
           );
           expect(res.body.data).to.be.null;
-          done();
         });
+    });
+
+    describe("Login User", () => {
+      it("should login a user if credential is valid", async () => {
+        await execUser();
+        const body = { email: userData.email, password: userData.password };
+        chai
+          .request(app)
+          .post(`${baseAPi}/login`)
+          .send(body)
+          .end((err, res) => {
+            expect(res.status).eql(200);
+            expect(res.body.message).eql("Login successful.");
+          });
+      });
+
+      it("should return 400 if user does not exist", async () => {
+        const body = { email: userData.email, password: userData.password };
+        chai
+          .request(app)
+          .post(`${baseAPi}/login`)
+          .send(body)
+          .end((err, res) => {
+            expect(res.status).eql(400);
+            expect(res.body.message).eql("User does not exist.");
+          });
+      });
+
+      it("should return 400 if password is invalid", async () => {
+        await execUser();
+        const body = { email: userData.email, password: invalidPassword };
+        chai
+          .request(app)
+          .post(`${baseAPi}/login`)
+          .send(body)
+          .end((err, res) => {
+            expect(res.status).eql(400);
+            expect(res.body.message).eql("Invalid email or password!");
+          });
+      });
     });
   });
 });
