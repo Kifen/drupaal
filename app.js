48d2ef6dc78c19ade11a3d8eae68186000637299
Kifen <kifenraph@gmail.com>
2020-04-10 18:47:17 +0000

    fix endpoint

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
 
 app.use(express.json({ limit: "50mb" }));
 app.use(morgan("combined", { stream: logger.stream }));
-app.use("/api", apiRoutes);
+app.use("/", apiRoutes);
 app.use(error);
 
 if (!config.get("jwtPrivateKey")) {
